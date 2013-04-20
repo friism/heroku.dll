5669a396f312ad82f975414e4183453ae21fb76e
Michael Friis <friism@gmail.com>
2013-04-19 22:55:29 -0700

    add integration tests

---
+++ Heroku.IntegrationTests/AccountTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+
+namespace Heroku.IntegrationTests
+{
+	public class AccountTests : TestClass
+	{
+		[Fact]
+		public void CanGetAccount()
+		{
+			var account = _client.Account.Get();
+			Assert.NotEqual(account.Id, default(Guid));
+			Assert.NotEqual(account.UpdatedAt, default(DateTime));
+			Assert.NotEqual(account.LastLogin, default(DateTime));
+			Assert.False(string.IsNullOrEmpty(account.Email));
+		}
+	}
+}
---
+++ Heroku.IntegrationTests/TestClass.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Heroku.IntegrationTests
+{
+	public class TestClass
+	{
+		protected readonly HerokuClient _client;
+
+		public TestClass()
+		{
+			_client = new HerokuClient();
+		}
+	}
+}
