e5ee8cb309b01a7c8c924aa786302470333209b7
Michael Friis <friism@gmail.com>
2013-05-03 15:16:22 -0700

    fix formation

--- Heroku/Model/Formation.cs
+++ Heroku/Model/Formation.cs
@@ -3,9 +3,9 @@
 	public class Formation : ResourceBase
 	{
 		public string Command { get; set; }
-		public string Name { get; set; }
 		public int Quantity { get; set; }
 		public int Size { get; set; }
+		public string Type { get; set; }
 
 		public class UpdateRequest : IUpdateRequest<Formation>
 		{
