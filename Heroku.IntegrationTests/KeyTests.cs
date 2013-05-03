c4227b711d4b02b596f4a10b92b485bca9a7bba4
Michael Friis <friism@gmail.com>
2013-05-03 15:14:40 -0700

    refactor key test

--- Heroku.IntegrationTests/KeyTests.cs
+++ Heroku.IntegrationTests/KeyTests.cs
@@ -5,7 +5,9 @@ namespace Heroku.IntegrationTests
 {
 	public class KeyTests : TestClass
 	{
-		private const string _key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCseVc/JLiVxXCFORJHf3zZjd3QExMW4DfVUfs/ZkELgAsxtmqs2wQQasG04iVVeOceZB1y1PjWGlsvEq+UKITE3H2gjOS/i+feTNMqfTys6Luhaz832XPJauM392kCchLAC1IqJPYCDN8ZYvzmFK/2Fi3y5YyZcPqYePpeeTFYGRitFmJ6vmMn3b/I1M6g8Mz+yiDSspwAopxjX/et8GWv3sVZmSVZvtxmmSY6UO8jSm5hKf5gy36jsYLMylr96w9mHvhi6bWHq4Bo8QR9mLrwISwaJdBYumVOg+LEz8H95UQIUZaqpvo3VYK7eOUgSul1DNejneftHRX2uiu839Uv username@example.com";
+		private const string _sshKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCseVc/JLiVxXCFORJHf3zZjd3QExMW4DfVUfs/ZkELgAsxtmqs2wQQasG04iVVeOceZB1y1PjWGlsvEq+UKITE3H2gjOS/i+feTNMqfTys6Luhaz832XPJauM392kCchLAC1IqJPYCDN8ZYvzmFK/2Fi3y5YyZcPqYePpeeTFYGRitFmJ6vmMn3b/I1M6g8Mz+yiDSspwAopxjX/et8GWv3sVZmSVZvtxmmSY6UO8jSm5hKf5gy36jsYLMylr96w9mHvhi6bWHq4Bo8QR9mLrwISwaJdBYumVOg+LEz8H95UQIUZaqpvo3VYK7eOUgSul1DNejneftHRX2uiu839Uv";
+		private const string _emailAddress = "username@example.com";
+		private readonly string _key = string.Format("{0} {1}", _sshKey, _emailAddress);
 
 		[Fact]
 		public void TestKeys()
@@ -14,6 +16,7 @@ namespace Heroku.IntegrationTests
 
 			TestResource(key);
 			Assert.Equal(_key, key.PublicKey);
+			Assert.Equal(_emailAddress, _emailAddress);
 
 			key = _client.Keys.Get(key.Id);
 
